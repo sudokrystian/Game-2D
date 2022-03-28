a63a4cc699503705b845a60fec6e1afe1c81b815
sudokrystian <48478528+sudokrystian@users.noreply.github.com>
2022-03-28 17:54:44 +0200

    Removed automatic AI, code implementation for AI

--- Assets/Scripts/EnemyBehaviour.cs
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -8,27 +8,42 @@ public class EnemyBehaviour : MonoBehaviour
 {
     // Enemy components
     private Rigidbody2D rigidBody;
+    public Transform enemyGFX;
     public GameObject bloodPrefab;
     [SerializeField] private HealthBar HealthBar;
     // Enemy stats
-    [SerializeField] private float EnemySpeed = 1f;
+    [SerializeField] private float EnemySpeed = 350f;
     [SerializeField] private float EnemyMaxHealth = 5;
     [SerializeField] private float EnemyDamage = 1;
-    
+    private float Hitpoints;
     // Collision info
     private float timeColliding = 0;
     // Time before damage is taken
     private float timeThreshold = 0.5f;
+    // Target for AI
+    public Transform target;
+    public float nextWaypointDistance = 3f;
+
+    private Path path;
+    private int currentWaypoint = 0;
+    private bool reachedEndOfPath = false;
+    private Seeker seeker;
     
-    private float Hitpoints;
     
     void Start()
     {
         rigidBody = GetComponent<Rigidbody2D>();
+        seeker = GetComponent<Seeker>();
         Hitpoints = EnemyMaxHealth;
         HealthBar.SetMaxHealth(EnemyMaxHealth);
+        InvokeRepeating("UpdatePath", 0f, 0.5f);
     }
-    
+
+    private void FixedUpdate()
+    {
+        MoveEnemyTowardsTarget();
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         var player = collision.collider.GetComponent<Character2DController>();
@@ -55,6 +70,60 @@ public class EnemyBehaviour : MonoBehaviour
             }
         }
     }
+
+    private void MoveEnemyTowardsTarget()
+    {
+        if (path == null)
+        {
+            return;
+        }
+
+        if (currentWaypoint >= path.vectorPath.Count)
+        {
+            reachedEndOfPath = true;
+            return;
+        }
+        else
+        {
+            reachedEndOfPath = false;
+        }
+        // Get the direction and force to move
+        Vector2 direction = ((Vector2) path.vectorPath[currentWaypoint] - (Vector2) rigidBody.position).normalized;
+        Vector2 force = direction * EnemySpeed * Time.deltaTime;
+        // Move the enemy
+        rigidBody.AddForce(force);
+        
+        float distance = Vector2.Distance(rigidBody.position, path.vectorPath[currentWaypoint]);
+        if (distance < nextWaypointDistance)
+        {
+            currentWaypoint++;
+        }
+        // Rotate the enemy accordingly
+        if (force.x >= 0.01f)
+        {
+            enemyGFX.localScale = new Vector3(-1f, 1f, 1f);
+        } else if (force.x <= -0.01f)
+        {
+            enemyGFX.localScale = new Vector3(1f, 1f, 1f);
+        }
+    }
+
+    private void UpdatePath()
+    {
+        if (seeker.IsDone())
+        {
+            seeker.StartPath(rigidBody.position, target.position, OnPathComplete);
+        }
+    }
+    
+    private void OnPathComplete(Path path)
+    {
+        if (!path.error)
+        {
+            this.path = path;
+            currentWaypoint = 0;
+        }
+    }
     
 
     public void TakeHit(float damage)
@@ -68,14 +137,4 @@ public class EnemyBehaviour : MonoBehaviour
             Destroy(gameObject);
         }
     }
-
-    private void CollideWithPlayer(Collision2D collisionInfo)
-    {
-        var player = collisionInfo.collider.GetComponent<Character2DController>();
-        if (player)
-        {
-            player.TakeHit(EnemyDamage);
-        }
-    }
-
 }

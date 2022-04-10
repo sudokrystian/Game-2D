ff70425954542194880dbe1d4c6d5fa0a41b78cc
sudokrystian <48478528+sudokrystian@users.noreply.github.com>
2022-04-10 21:37:41 +0200

    Enemies patrolling added

--- Assets/Map1/Scripts/Enemies/EnemyAI.cs
+++ Assets/Map1/Scripts/Enemies/EnemyAI.cs
@@ -10,14 +10,16 @@ public class EnemyAI : MonoBehaviour
 {
     // Enemy
     public EnemyStats enemyStats;
+
     // How far before the enemy can see the player
     [SerializeField] private float visionRange = 8;
+
     // Enemy flags
     [SerializeField] private bool canFly = false;
     private bool canJump = false;
     private bool confused = false;
     private Vector2 randomDirection;
-    
+
     // Target for AI
     private Transform target;
     [SerializeField] private float nextWaypointDistance = 1f;
@@ -26,6 +28,7 @@ public class EnemyAI : MonoBehaviour
     private int currentWaypoint = 0;
     private bool reachedEndOfPath = false;
     private Seeker seeker;
+
     void Start()
     {
         target = GameObject.FindWithTag("PlayerHitbox").GetComponent<RectTransform>();
@@ -42,7 +45,7 @@ public class EnemyAI : MonoBehaviour
     {
         MoveEnemyTowardsTarget();
     }
-    
+
     private void MoveEnemyTowardsTarget()
     {
         if (path == null)
@@ -59,103 +62,112 @@ public class EnemyAI : MonoBehaviour
         {
             reachedEndOfPath = false;
         }
-        // Get the direction and force to move
-        Vector2 direction;
-        Vector2 force;
+        
         // Flying enemies
         if (canFly)
         {
-            direction = ((Vector2) path.vectorPath[currentWaypoint] - (Vector2) enemyStats.RigidBody.position).normalized;
-            force = direction * enemyStats.EnemySpeed * Time.deltaTime;
-            // Move the enemy if he is in range
-            float distanceFromPlayer = Vector2.Distance(enemyStats.RigidBody.position, target.position);
-            if (distanceFromPlayer < visionRange)
-            {
-                enemyStats.RigidBody.AddForce(force);
-            }
-        
-            float distance = Vector2.Distance(enemyStats.RigidBody.position, path.vectorPath[currentWaypoint]);
-            if (distance < nextWaypointDistance)
-            {
-                currentWaypoint++;
-            }
-
-            if (enemyStats.RigidBody.velocity.x >= 0.01f)
-            {
-                enemyStats.enemyGFX.transform.rotation = Quaternion.Euler(0, 180, 0);
-
-            } else if (enemyStats.RigidBody.velocity.x <= -0.01f)
-            {
-                enemyStats.enemyGFX.transform.rotation = Quaternion.identity;
-            }
+            FlyingEnemiesPathFinding();
         }
         // Non-flying enemies
         else
         {
-            // enemyStats.RigidBody.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+            NonFlyingEnemiesPathFinding();
+        }
+    }
 
-            var x = path.vectorPath[currentWaypoint].x - enemyStats.RigidBody.position.x;
-            direction = new Vector2(x, 0).normalized;
-            force = direction * enemyStats.EnemySpeed * Time.deltaTime;
- 
-            // Move the enemy if he is in range
-            float distanceFromPlayer = Vector2.Distance(enemyStats.RigidBody.position, target.position);
-            if (distanceFromPlayer < visionRange)
+    private void FlyingEnemiesPathFinding()
+    {
+        // Get the direction and force to move
+        var direction = ((Vector2) path.vectorPath[currentWaypoint] - (Vector2) enemyStats.RigidBody.position)
+            .normalized;
+        var force = direction * enemyStats.EnemySpeed * Time.deltaTime;
+        // Move the enemy if he is in range
+        float distanceFromPlayer = Vector2.Distance(enemyStats.RigidBody.position, target.position);
+        if (distanceFromPlayer < visionRange)
+        {
+            enemyStats.RigidBody.AddForce(force);
+        }
+
+        float distance = Vector2.Distance(enemyStats.RigidBody.position, path.vectorPath[currentWaypoint]);
+        if (distance < nextWaypointDistance)
+        {
+            currentWaypoint++;
+        }
+
+        if (enemyStats.RigidBody.velocity.x >= 0.01f)
+        {
+            enemyStats.enemyGFX.transform.rotation = Quaternion.Euler(0, 180, 0);
+        }
+        else if (enemyStats.RigidBody.velocity.x <= -0.01f)
+        {
+            enemyStats.enemyGFX.transform.rotation = Quaternion.identity;
+        }
+    }
+
+    private void NonFlyingEnemiesPathFinding()
+    {
+        // Get the direction and force to move
+        var x = path.vectorPath[currentWaypoint].x - enemyStats.RigidBody.position.x;
+        var direction = new Vector2(x, 0).normalized;
+        var force = direction * enemyStats.EnemySpeed * Time.deltaTime;
+
+        // Move the enemy if he is in range
+        float distanceFromPlayer = Vector2.Distance(enemyStats.RigidBody.position, target.position);
+        if (distanceFromPlayer < visionRange)
+        {
+            // If the enemy is above you and you can jump let's jump
+            if (path.vectorPath[currentWaypoint].y > (enemyStats.RigidBody.position.y + 1f) && canJump)
             {
-                // If the enemy is above you and you can jump let's jump
-                if (path.vectorPath[currentWaypoint].y > (enemyStats.RigidBody.position.y + 1f) && canJump)
+                Vector2 jumpDirection = new Vector2(0, enemyStats.JumpForce);
+                enemyStats.RigidBody.AddForce(jumpDirection, ForceMode2D.Impulse);
+                this.canJump = false;
+            }
+            // Otherwise let's walk
+            else
+            {
+                if (Math.Abs(path.vectorPath[currentWaypoint].x - (enemyStats.RigidBody.position.x)) > 0.5f)
                 {
-                    Vector2 jumpDirection = new Vector2(0, enemyStats.JumpForce);
-                    enemyStats.RigidBody.AddForce(jumpDirection, ForceMode2D.Impulse);
-                    this.canJump = false;
+                    confused = false;
+                    enemyStats.RigidBody.AddForce(force);
                 }
-                // Otherwise let's walk
+                // If the enemy is right underneath try walking left and right to find a new path
                 else
                 {
-                    if (Math.Abs(path.vectorPath[currentWaypoint].x - (enemyStats.RigidBody.position.x)) > 0.5f)
+                    if (!confused)
                     {
-                        confused = false;
-                        enemyStats.RigidBody.AddForce(force);
+                        float randomX = Random.Range(-1f, 1f);
+                        randomDirection = new Vector2(randomX, 0).normalized;
                     }
-                    // If the enemy is right underneath try walking left and right to find a new path
-                    else
-                    {
-                        if (!confused)
-                        {
-                            float randomX = Random.Range(-1f, 1f);
-                            randomDirection = new Vector2(randomX, 0).normalized;
 
-                        }
-                        confused = true;
-                        Vector2 alternativeForce = randomDirection * enemyStats.EnemySpeed * Time.deltaTime;
-                        enemyStats.RigidBody.AddForce(alternativeForce);
-                    }
+                    confused = true;
+                    Vector2 alternativeForce = randomDirection * enemyStats.EnemySpeed * Time.deltaTime;
+                    enemyStats.RigidBody.AddForce(alternativeForce);
                 }
-            }
-        
-            float distance = Vector2.Distance(enemyStats.RigidBody.position, path.vectorPath[currentWaypoint]);
-            if (distance < nextWaypointDistance)
-            {
-                // Make sure that we won't go out of bounds when the enemy is close ot the destination
-                if (currentWaypoint < path.vectorPath.Count - 1)
-                {
-                    currentWaypoint++;
-                }
-            }
-
-            if (enemyStats.RigidBody.velocity.x >= 0.01f && (Math.Abs(path.vectorPath[currentWaypoint].x - enemyStats.RigidBody.position.x) > 0.1f))
-            {
-                enemyStats.enemyGFX.transform.rotation = Quaternion.Euler(0, 180, 0);
-
-            } else if (enemyStats.RigidBody.velocity.x <= -0.01f && Math.Abs(path.vectorPath[currentWaypoint].x - enemyStats.RigidBody.position.x) > 0.1f)
-            {
-                enemyStats.enemyGFX.transform.rotation = Quaternion.identity;
             }
         }
-        
-        
+
+        float distance = Vector2.Distance(enemyStats.RigidBody.position, path.vectorPath[currentWaypoint]);
+        if (distance < nextWaypointDistance)
+        {
+            // Make sure that we won't go out of bounds when the enemy is close ot the destination
+            if (currentWaypoint < path.vectorPath.Count - 1)
+            {
+                currentWaypoint++;
+            }
+        }
+
+        if (enemyStats.RigidBody.velocity.x >= 0.01f &&
+            (Math.Abs(path.vectorPath[currentWaypoint].x - enemyStats.RigidBody.position.x) > 0.1f))
+        {
+            enemyStats.enemyGFX.transform.rotation = Quaternion.Euler(0, 180, 0);
+        }
+        else if (enemyStats.RigidBody.velocity.x <= -0.01f &&
+                 Math.Abs(path.vectorPath[currentWaypoint].x - enemyStats.RigidBody.position.x) > 0.1f)
+        {
+            enemyStats.enemyGFX.transform.rotation = Quaternion.identity;
+        }
     }
-    
+
     private void UpdatePath()
     {
         if (seeker.IsDone())
@@ -163,7 +175,7 @@ public class EnemyAI : MonoBehaviour
             seeker.StartPath(enemyStats.RigidBody.position, target.position, OnPathComplete);
         }
     }
-    
+
     private void OnPathComplete(Path path)
     {
         if (!path.error)
@@ -177,7 +189,7 @@ public class EnemyAI : MonoBehaviour
     {
         if (Mathf.Abs(enemyStats.RigidBody.velocity.y) < 0.001f)
         {
-            canJump= true;
+            canJump = true;
         }
     }
 }
